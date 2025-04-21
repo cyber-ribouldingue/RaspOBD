1543e3f93464ca49e474df2e71c3a74d2c8e5489
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-21 14:47:16 +0200

    Update configure_display.sh

--- scripts/configure_display.sh
+++ scripts/configure_display.sh
@@ -2,11 +2,11 @@
 
 echo "Configuration écran TFT Joy-IT 3.2..."
 
-# Ajouter au config.txt
+# Ajout au config.txt
 sudo tee -a /boot/config.txt > /dev/null <<EOT
-# Configuration pour écran TFT Joy-IT 3.2
-dtoverlay=spi1-1cs
+# RaspOBD Display config
+dtoverlay=spi0-0cs
 dtoverlay=fb_ili9341,spi0-0,rotate=270,speed=32000000,dc_pin=24,reset_pin=25
 EOT
 
-echo "Ecran TFT configuré !"
+echo "Écran configuré. Redémarrage nécessaire après installation."
