a36e7900923f2caefac97e7248512a238ca92815
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-21 14:48:16 +0200

    Update config.py

--- interface/config.py
+++ interface/config.py
@@ -1,7 +1,9 @@
-# GPIO Button Mapping
+# Fichier de configuration générale
+
+# GPIO mapping des boutons
 BUTTON_UP = 17
 BUTTON_DOWN = 27
 BUTTON_SELECT = 22
 
-# Bluetooth OBD device name (par défaut)
+# Bluetooth
 OBD_DEVICE_NAME = "OBDII"
